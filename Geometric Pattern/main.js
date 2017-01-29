060487f6207723c18108e9bbca7e6e85d838f439
Caleb Nelson <caleb.quade.nelson@gmail.com>
2017-01-29 03:36:16 +0000

    Create Doge Game

--- Geometric Pattern/main.js
+++ Geometric Pattern/main.js
@@ -1,9 +1,15 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
 
+var rVal;
+var gVal;
+var bVal;
+
 function setup() {
   createCanvas(480, 600);
+
   circleDiameter = width/NUM_CIRCLES;
   circleRadius = circleDiameter/2;
 }
@@ -11,16 +17,43 @@ function setup() {
 function draw() {
   var isShifted = false;
 
-  var y = 0;
-  while (y <= height) {
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
 
-    var x = 0;
+  var y = height;
+  while (y >= 0) {
+
+    var x;
+
+    if (isShifted) {
+      x = circleRadius;
+    } else {
+      x = 0;
+    }
+
     while (x <= width) {
+      fill(color(rVal, gVal, bVal));
+      stroke(color(rVal, gVal, bVal));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
 
-    y = y + circleRadius;
+    stroke(color(rVal, gVal, bVal));
+    fill(color(rVal, gVal, bVal));
+
+    y = y - circleRadius;
     isShifted = !isShifted;
+
+    rVal = (rVal - 2) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) % 256;
   }
+}
+
+function keyPressed() {
+  if (keyCode === 115 || keyCode === 83) {
+    saveCanvas('geometricPattern', 'png');
+  }
+  return false;
 }
